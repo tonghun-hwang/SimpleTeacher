f337929b226866f34eca1963f938a1a2dfd84ffa
sophia <fragrance0627@gmail.com>
2020-01-31 16:07:29 +0100

    Add training database as a test version
    Signed-off-by: sophia <fragrance0627@gmail.com>

---
+++ app/src/main/java/com/example/simpleteacher/helper/userResultDBHelper.java
@@ -0,0 +1,209 @@
+package com.example.simpleteacher.helper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.simpleteacher.ItemActivity;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class userResultDBHelper extends SQLiteOpenHelper {
+    public static final String TABLE_BASE = "table_base";
+    public static final String TABLE_SESSION_LAST_BASE = "table_last_base";
+    public static final String RESULT_DIAGNOSTIC = "result_diagnostic";
+    public static final String RESULT_DIAG_DETAIL = "result_diag_detail";
+    public static final String RESULT_DIAG_MAIN = "result_diag_main";
+
+    public static final String ID = "ID";
+    public static final String CORRECT_WORD = "CORRECT_WORD";
+    public static final String WRITTEN_WORD = "WRITTEN_WORD";
+    public static final String CATEGORY_TOTAL = "CATEGORY_TOTAL";
+    public static final String CATEGORY_ERROR = "CATEGORY_ERROR";
+    public static final String UNCATEGORIZED = "UNCATEGORIZED";
+    public static final String MULTI_BIT = "MULTI_BIT";
+    public static final String CATEGORY_ERROR_RAW = "CATEGORY_ERROR_RAW";
+    public static final String DATE = "LAST_UPDATE";
+
+    public static final String SESSION = "SESSION";
+    public static final String CATEGORY_MAIN = "CATEGORY_MAIN";
+    public static final String CATEGORY_SUB = "CATEGORY_SUB";
+    public static final String NUM_ERROR = "NUM_ERROR";
+    public static final String NUM_TOTAL = "NUM_TOTAL";
+    public static final String ERROR_RATE = "ERROR_RATE";
+
+    public static final String WORD = "WORD";
+    public static final String CATEGORY = "CATEGORY";
+    public static final String DIFFICULT = "DIFFICULT";
+
+    private static final String TAG = "Main.userResultDBHelper";
+    private ItemActivity mParent;
+
+    public userResultDBHelper(Context context, String dbName, int version) {
+        super(context, dbName, null, version);
+        mParent = (ItemActivity) context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        createSimpleWordListTable(db, TABLE_BASE);
+        createSimpleWordListTable(db, TABLE_SESSION_LAST_BASE);
+        createWordListTable(db, RESULT_DIAGNOSTIC);
+        createCategoryResultTable(db, RESULT_DIAG_DETAIL);
+        createCategoryResultTable(db, RESULT_DIAG_MAIN);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + RESULT_DIAGNOSTIC);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_BASE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SESSION_LAST_BASE);
+        onCreate(db);
+    }
+
+    public void createWordListTable(SQLiteDatabase db, String tableName) {
+        db.execSQL("DROP TABLE IF EXISTS " + tableName);
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + tableName +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "CORRECT_WORD TEXT," +
+                "WRITTEN_WORD TEXT," +
+                "CATEGORY_TOTAL INTEGER," +
+                "CATEGORY_ERROR INTEGER," +
+                "UNCATEGORIZED INTEGER," +
+                "MULTI_BIT INTEGER," +
+                "CATEGORY_ERROR_RAW TEXT," +
+                "LAST_UPDATE DATETIME);");
+    }
+
+    public void createCategoryResultTable(SQLiteDatabase db, String tableName) {
+        db.execSQL("DROP TABLE IF EXISTS " + tableName);
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + tableName +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "SESSION INTEGER," +
+                "CATEGORY_MAIN INTEGER," +
+                "CATEGORY_SUB INTEGER," +
+                "NUM_ERROR INTEGER," +
+                "NUM_TOTAL INTEGER," +
+                "ERROR_RATE REAL," +
+                "LAST_UPDATE DATETIME);");
+    }
+
+    public void createMainCategoryResultTable(SQLiteDatabase db, String tableName) {
+        db.execSQL("DROP TABLE IF EXISTS " + tableName);
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + tableName +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "SESSION INTEGER," +
+                "CATEGORY_MAIN INTEGER," +
+                "CATEGORY_SUB INTEGER," +
+                "NUM_ERROR INTEGER," +
+                "NUM_TOTAL INTEGER," +
+                "ERROR_RATE REAL," +
+                "LAST_UPDATE DATETIME);");
+    }
+
+    // TODO: erase id
+    public boolean replaceData(SQLiteDatabase db, String[] data, int id) {
+        Log.d(TAG, "replaceData" );
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String update = formatter.format(Calendar.getInstance().getTimeInMillis());
+        long res = -1;
+        if (data.length > 0) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(ID, id + 1);
+            contentValues.put(CORRECT_WORD, data[0]);
+            contentValues.put(WRITTEN_WORD, data[1]);
+            contentValues.put(CATEGORY_TOTAL, Integer.valueOf(data[2]));
+            contentValues.put(CATEGORY_ERROR, Integer.valueOf(data[3]));
+            contentValues.put(UNCATEGORIZED, Integer.valueOf(data[4]));
+            contentValues.put(MULTI_BIT, Integer.valueOf(data[5]));
+            contentValues.put(CATEGORY_ERROR_RAW, data[6]);
+            contentValues.put(DATE, update);
+            res = db.replace(RESULT_DIAGNOSTIC,
+                    null, contentValues);
+        }
+        if (res == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // TODO: erase id
+    public boolean replaceSummaryData(SQLiteDatabase db, String tableName, int[] num, int id) {
+        Log.d(TAG, "replaceSummaryData");
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String update = formatter.format(Calendar.getInstance().getTimeInMillis());
+        double errorRate = (double) num[3] / num[4];
+
+        Log.d(TAG, "errorRate: " + errorRate);
+        long res = -1;
+        if (num.length > 0) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(ID, id + 1);
+            contentValues.put(SESSION, num[0]);
+            contentValues.put(CATEGORY_MAIN, num[1]);
+            contentValues.put(CATEGORY_SUB, num[2]);
+            contentValues.put(NUM_ERROR, num[3]);
+            contentValues.put(NUM_TOTAL, num[4]);
+            contentValues.put(ERROR_RATE, errorRate);
+            contentValues.put(DATE, update);
+            res = db.replace(tableName,
+                    null, contentValues);
+        }
+        if (res == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void createSimpleWordListTable(SQLiteDatabase db, String tableName) {
+        db.execSQL("DROP TABLE IF EXISTS " + tableName);
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + tableName +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "WORD TEXT," +
+                "CATEGORY INTEGER," +
+                "DIFFICULT REAL," +
+                "LAST_UPDATE DATETIME);");
+    }
+
+    public boolean replaceSimpleData(SQLiteDatabase db, String tableName, String word, double difficult, int[] integers) {
+        Log.d(TAG, "replaceData (mode): ");
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String update = formatter.format(Calendar.getInstance().getTimeInMillis());
+        long res = -1;
+        if (word != null) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(ID, integers[0]);
+            contentValues.put(WORD, word);
+            contentValues.put(CATEGORY, integers[1]);
+            contentValues.put(DIFFICULT, difficult);
+            contentValues.put(DATE, update);
+            res = db.replace(tableName,
+                    null, contentValues);
+        }
+        if (res == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public int deleteRowById(SQLiteDatabase db, String tableName, int id) {
+        Log.d(TAG, "deleteRowById(): ");
+
+        int res = 0;
+        if (id > 0) {
+            res = db.delete(tableName, "ID = ?", new String[] {String.valueOf(id)});
+        }
+        return res;
+    }
+}
