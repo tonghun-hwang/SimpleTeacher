7882b9f1465b33e64fb5a3fbb22e2ccc01efce48
tonghun.hwang@sportwiss.uni-hannover.de <tonghun.hwang@sportwiss.uni-hannover.de>
2020-06-16 13:56:56 +0200

    record the number of errors at every trials

--- app/src/main/java/com/example/simpleteacher/asyncTask/TrainingAnalysisTask.java
+++ app/src/main/java/com/example/simpleteacher/asyncTask/TrainingAnalysisTask.java
@@ -47,8 +47,199 @@ public class TrainingAnalysisTask extends AsyncTask <String, Void, String> {
         int offset = 0;
 
         for (int i = 0; i < mIds.length; i++) {
-            offset = 0;
             try {
+                offset = 0;
+                for (int j = 1; j <= 16; j++) {
+                    if (j % 5 == 1) {
+                        for (int k = 0; k < data.length; k++) {
+                            data[k] = 0;
+                        }
+                        total = 0;
+                        totalerr = 0;
+                        cat = 0;
+                        caterr = 0;
+                    }
+                    /* result of buttons */
+                    dbName = "training_" + mIds[i] + "_" + j + ".db";
+                    mParent.mUserTrainingDBHelper = new userTrainingDBHelper(mContext, dbName, mIds[i], 1);
+                    mParent.mUserTrainingDB = mParent.mUserTrainingDBHelper.getWritableDatabase();
+
+                    total = mParent.getNumTotalWords(mIds[i]);
+                    if (total == 0) {
+                        mParent.mUserTrainingDB.close();
+                        mParent.mUserTrainingDBHelper.close();
+
+                        dbName = "training_" + mIds[i] + "_" + j + "ps.db";
+                        mParent.mUserTrainingDBHelper = new userTrainingDBHelper(mContext, dbName, mIds[i], 1);
+                        mParent.mUserTrainingDB = mParent.mUserTrainingDBHelper.getWritableDatabase();
+                    }
+
+                    /* get the errors at first third trial */
+                    totalerr = mParent.getNumFirstWrongWords(mIds[i]);
+                    cat = mParent.getNumFirstWrongWordsInCategory(mIds[i]);
+                    caterr = mParent.getNumTotallyWrongWordsInCategory(mIds[i]);
+
+                    data[0] += total;
+                    data[(j * 3) + 5 - offset] = total;
+
+                    data[1] += totalerr;
+                    data[2] += cat;
+                    data[3] += caterr; // wrong words
+
+                    if (total != 0) {
+                        data[(j * 3) + 6 - offset] = (double) totalerr / total * 100;
+                    } else {
+                        data[(j * 3) + 6 - offset] = 0;
+                    }
+
+                    if (cat != 0) {
+                        data[(j * 3) + 7 - offset] = (double) caterr / cat * 100;
+                    } else {
+                        data[(j * 3) + 7 - offset] = 0;
+                    }
+
+                    data[4] += mParent.getNumEraseAll(mIds[i]);
+                    data[5] += mParent.getNumEraseOne(mIds[i]);
+                    data[6] += mParent.getNumButtonEar(mIds[i]);
+                    data[7] += mParent.getNumPushChar(mIds[i]);
+
+                    if (j % 5 == 0) {
+                        if (j < 0 && j <= 5) {
+                            sessionBlock = 1;
+                            offset = 15;
+                        } else if (j > 5 && j <= 10) {
+                            sessionBlock = 2;
+                            offset = 30;
+                        } else if (j > 10 && j <= 15) {
+                            sessionBlock = 3;
+                            offset = 45;
+                        } else {
+                            sessionBlock = 1;
+                            offset = 15;
+                        }
+                        /* replace the database */
+                        if (mParent.mAnalysisTrainingDB == null || !mParent.mAnalysisTrainingDB.isOpen()) {
+                            mParent.mAnalysisTrainingDB = mParent.mAnalysisTrainingDBHelper.getWritableDatabase();
+                        }
+                        mParent.mAnalysisTrainingDBHelper.replaceData(mParent.mAnalysisTrainingDB, 3 * i, mIds[i], data, sessionBlock);
+
+                    } else if (j == 16) {
+                        sessionBlock = 4;
+
+                        /* replace the database */
+                        if (mParent.mAnalysisTrainingDB == null || !mParent.mAnalysisTrainingDB.isOpen()) {
+                            mParent.mAnalysisTrainingDB = mParent.mAnalysisTrainingDBHelper.getWritableDatabase();
+                        }
+                        mParent.mAnalysisTrainingDBHelper.replaceData(mParent.mAnalysisTrainingDB, 3 * i, mIds[i], data, sessionBlock);
+                    }
+                    if (mParent.mUserTrainingDB != null) {
+                        if (mParent.mUserTrainingDB.isOpen()) {
+                            mParent.mUserTrainingDB.close();
+                        }
+                    }
+                    if (mParent.mUserTrainingDBHelper != null) {
+                        mParent.mUserTrainingDBHelper.close();
+                        mParent.mUserTrainingDBHelper = null;
+                    }
+                }
+
+                offset = 0;
+                for (int j = 1; j <= 16; j++) {
+                    if (j % 5 == 1) {
+                        for (int k = 0; k < data.length; k++) {
+                            data[k] = 0;
+                        }
+                        total = 0;
+                        totalerr = 0;
+                        cat = 0;
+                        caterr = 0;
+                    }
+                    /* result of buttons */
+                    dbName = "training_" + mIds[i] + "_" + j + ".db";
+                    mParent.mUserTrainingDBHelper = new userTrainingDBHelper(mContext, dbName, mIds[i], 1);
+                    mParent.mUserTrainingDB = mParent.mUserTrainingDBHelper.getWritableDatabase();
+
+                    total = mParent.getNumTotalWords(mIds[i]);
+                    if (total == 0) {
+                        mParent.mUserTrainingDB.close();
+                        mParent.mUserTrainingDBHelper.close();
+
+                        dbName = "training_" + mIds[i] + "_" + j + "ps.db";
+                        mParent.mUserTrainingDBHelper = new userTrainingDBHelper(mContext, dbName, mIds[i], 1);
+                        mParent.mUserTrainingDB = mParent.mUserTrainingDBHelper.getWritableDatabase();
+                    }
+
+                    /* get the errors at second trial */
+                    totalerr = mParent.getNumSecondWrongWords(mIds[i]);
+                    cat = mParent.getNumTotalWordsInCategory(mIds[i]);
+                    caterr = mParent.getNumSecondWrongWordsInCategory(mIds[i]);
+
+                    data[0] += total;
+                    data[(j * 3) + 5 - offset] = total;
+
+                    data[1] += totalerr;
+                    data[2] += cat;
+                    data[3] += caterr; // wrong words
+
+                    if (total != 0) {
+                        data[(j * 3) + 6 - offset] = (double) totalerr / total * 100;
+                    } else {
+                        data[(j * 3) + 6 - offset] = 0;
+                    }
+
+                    if (cat != 0) {
+                        data[(j * 3) + 7 - offset] = (double) caterr / cat * 100;
+                    } else {
+                        data[(j * 3) + 7 - offset] = 0;
+                    }
+
+                    data[4] += mParent.getNumEraseAll(mIds[i]);
+                    data[5] += mParent.getNumEraseOne(mIds[i]);
+                    data[6] += mParent.getNumButtonEar(mIds[i]);
+                    data[7] += mParent.getNumPushChar(mIds[i]);
+
+                    if (j % 5 == 0) {
+                        if (j < 0 && j <= 5) {
+                            sessionBlock = 1;
+                            offset = 15;
+                        } else if (j > 5 && j <= 10) {
+                            sessionBlock = 2;
+                            offset = 30;
+                        } else if (j > 10 && j <= 15) {
+                            sessionBlock = 3;
+                            offset = 45;
+                        } else {
+                            sessionBlock = 1;
+                            offset = 15;
+                        }
+                        /* replace the database */
+                        if (mParent.mAnalysisTrainingDB == null || !mParent.mAnalysisTrainingDB.isOpen()) {
+                            mParent.mAnalysisTrainingDB = mParent.mAnalysisTrainingDBHelper.getWritableDatabase();
+                        }
+                        mParent.mAnalysisTrainingDBHelper.replaceData(mParent.mAnalysisTrainingDB, 3 * i + 1, mIds[i], data, sessionBlock);
+
+                    } else if (j == 16) {
+                        sessionBlock = 4;
+
+                        /* replace the database */
+                        if (mParent.mAnalysisTrainingDB == null || !mParent.mAnalysisTrainingDB.isOpen()) {
+                            mParent.mAnalysisTrainingDB = mParent.mAnalysisTrainingDBHelper.getWritableDatabase();
+                        }
+                        mParent.mAnalysisTrainingDBHelper.replaceData(mParent.mAnalysisTrainingDB, 3 * i + 1, mIds[i], data, sessionBlock);
+                    }
+
+                    if (mParent.mUserTrainingDB != null) {
+                        if (mParent.mUserTrainingDB.isOpen()) {
+                            mParent.mUserTrainingDB.close();
+                        }
+                    }
+                    if (mParent.mUserTrainingDBHelper != null) {
+                        mParent.mUserTrainingDBHelper.close();
+                        mParent.mUserTrainingDBHelper = null;
+                    }
+                }
+
+                offset = 0;
                 for (int j = 1; j <= 16; j++) {
                     if (j % 5 == 1 ) {
                         for (int k = 0; k < data.length; k++) {
@@ -73,7 +264,7 @@ public class TrainingAnalysisTask extends AsyncTask <String, Void, String> {
                         mParent.mUserTrainingDBHelper = new userTrainingDBHelper(mContext, dbName, mIds[i], 1);
                         mParent.mUserTrainingDB = mParent.mUserTrainingDBHelper.getWritableDatabase();
                     }
-
+                    /* get the errors at third trial */
                     totalerr = mParent.getNumTotallyWrongWords(mIds[i]);
                     cat = mParent.getNumTotalWordsInCategory(mIds[i]);
                     caterr = mParent.getNumTotallyWrongWordsInCategory(mIds[i]);
@@ -120,7 +311,7 @@ public class TrainingAnalysisTask extends AsyncTask <String, Void, String> {
                         if (mParent.mAnalysisTrainingDB == null || !mParent.mAnalysisTrainingDB.isOpen()) {
                             mParent.mAnalysisTrainingDB = mParent.mAnalysisTrainingDBHelper.getWritableDatabase();
                         }
-                        mParent.mAnalysisTrainingDBHelper.replaceData(mParent.mAnalysisTrainingDB, i, mIds[i], data, sessionBlock);
+                        mParent.mAnalysisTrainingDBHelper.replaceData(mParent.mAnalysisTrainingDB, 3 * i + 2, mIds[i], data, sessionBlock);
 
                     } else if (j == 16) {
                         sessionBlock = 4;
@@ -129,7 +320,7 @@ public class TrainingAnalysisTask extends AsyncTask <String, Void, String> {
                         if (mParent.mAnalysisTrainingDB == null || !mParent.mAnalysisTrainingDB.isOpen()) {
                             mParent.mAnalysisTrainingDB = mParent.mAnalysisTrainingDBHelper.getWritableDatabase();
                         }
-                        mParent.mAnalysisTrainingDBHelper.replaceData(mParent.mAnalysisTrainingDB, i, mIds[i], data, sessionBlock);
+                        mParent.mAnalysisTrainingDBHelper.replaceData(mParent.mAnalysisTrainingDB, 3 * i + 2, mIds[i], data, sessionBlock);
                     }
 
                     if (mParent.mUserTrainingDB != null) {
